78880fd13b7efa308cae1f0162c1fe88dd970f0a
Remy van Elst <relst@relst.nl>
2013-04-13 17:26:46 +0200

    Added changes from Jeroen

--- check_crl/check_crl.py
+++ check_crl/check_crl.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-# Remy van Elst - raymii.org - 2012 
+# Jeroen Nijhof <jnijhof@digidentity.eu>
+# Changelog: - fixed timezone bug by comparing GMT with GMT
+#            - changed hours to minutes for even more precision
+
+# Remy van Elst - raymii.org - 2012
 # 05.11.2012
 # Changelog: - check with hours instead of dates for more precision,
 #            - URL errors are now also catched as nagios exit code.
@@ -8,6 +12,7 @@
 # Michele Baldessari - Leitner Technologies - 2011
 # 23.08.2011
 
+import time
 import datetime
 import getopt
 import os
@@ -17,7 +22,7 @@ import sys
 import tempfile
 import urllib.request, urllib.parse, urllib.error
 
-def check_crl(url, warnhour, crithour):
+def check_crl(url, warn, crit):
     tmpcrl = tempfile.mktemp("crl")
     #request = urllib.request.urlretrieve(url, tmpcrl)
     try:
@@ -29,60 +34,56 @@ def check_crl(url, warnhour, crithour):
     ret = subprocess.check_output(["/usr/bin/openssl", "crl", "-inform", "DER", "-noout", "-nextupdate", "-in", tmpcrl])
     nextupdate = ret.strip().decode('utf-8').split("=")
     os.remove(tmpcrl)
-    eol = datetime.datetime.strptime(nextupdate[1],"%b %d %H:%M:%S %Y GMT")
-    today = datetime.datetime.now()
-    delta = eol - today
-    deltaseconds = delta.seconds + delta.days * 86400
-    expdays = delta.days
-    exphours = delta.seconds // 3600
-    hours = deltaseconds // 3600
-    if  hours > crithour and hours <= warnhour:
-        msg = "WARNING CRL Expires in %s hours (%s days) (on %s)" % (hours, expdays, eol)
+    eol = time.mktime(time.strptime(nextupdate[1],"%b %d %H:%M:%S %Y GMT"))
+    today = time.mktime(datetime.datetime.utcnow().timetuple())
+    seconds = eol - today
+    minutes = int(seconds / 60)
+    if  minutes > crit and minutes <= warn:
+        msg = "WARNING CRL Expires in %s minutes (on %s GMT)" % (minutes, time.asctime(time.localtime(eol)))
         exitcode = 1
-    elif hours < crithour:
-        msg = "CRITICAL CRL Expires in %s hours (%s  days ) (on %s)" % (hours, expdays, eol)
+    elif minutes <= crit:
+        msg = "CRITICAL CRL Expires in %s minutes (on %s GMT)" % (minutes, time.asctime(time.localtime(eol)))
         exitcode = 2
     else:
-        msg = "OK CRL Expires in %s hours (%s days) (on %s)" % (hours, expdays, eol)
+        msg = "OK CRL Expires in %s minutes (on %s GMT)" % (minutes, time.asctime(time.localtime(eol)))
         exitcode = 0
 
     print (msg)
     sys.exit(exitcode)
 
 def usage():
-    print ("check_crl.py -h|--help -v|--verbose -u|--url=<url> -w|--warninghours=<hours> -c|--criticalhours=<hours>")
-    print ("Example, if you want to get a warning if a CRL expires in 24 hours and a critical if it expires in 12 hours:")
-    print ("./check_crl.py -u \"http://domain.tld/url/crl.crl\" -w 24 -c 12")
+    print ("check_crl.py -h|--help -v|--verbose -u|--url=<url> -w|--warning=<minutes> -c|--critical=<minutes>")
+    print ("")
+    print ("Example, if you want to get a warning if a CRL expires in 8 hours and a critical if it expires in 6 hours:")
+    print ("./check_crl.py -u \"http://domain.tld/url/crl.crl\" -w 480 -c 360")
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hu:w:c:", ["help", "url=", "warninghours=", "criticalhours="])
+        opts, args = getopt.getopt(sys.argv[1:], "hu:w:c:", ["help", "url=", "warning=", "critical="])
     except getopt.GetoptError as err:
         usage()
         sys.exit(2)
     url = None
-    warningdays = None
-    criticaldays = None
-    warninghours = None
-    criticalhours = None
+    warning = None
+    critical = None
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit()
         elif o in ("-u", "--url"):
             url = a
-        elif o in ("-w", "--warninghours"):
-            warninghours = a
-        elif o in ("-c", "--criticalhours"):
-            criticalhours = a
+        elif o in ("-w", "--warning"):
+            warning = a
+        elif o in ("-c", "--critical"):
+            critical = a
         else:
             assert False, "unhandled option"
 
-    if url != None and warninghours != None and criticalhours != None:
-        check_crl(url, int(warninghours), int(criticalhours))
+    if url != None and warning != None and critical != None:
+        check_crl(url, int(warning), int(critical))
     else:
         usage()
-        sys.exit(2) 
+        sys.exit(2)
 
 
 if __name__ == "__main__":
